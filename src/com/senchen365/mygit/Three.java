85f078854ef15b52f4b6c43d203c280b71cd68ed
mingzitaichangtinghao <1183538946@qq.com>
2019-04-27 18:22:40 +0800

    活动结束·

---
+++ src/com/senchen365/mygit/Three.java
@@ -0,0 +1,17 @@
+package com.senchen365.mygit;
+
+/**
+ * 类名:Three<br>
+ * 功能:<br>
+ * 作者:李帅<br>
+ * 日期:2019/4/27<br>
+ * 版本:1.0.0<br>
+ */
+public class Three {
+    /**
+     * 方法主入口
+     */
+    public static void main(String[]arge){
+        System.out.println("最近你还好吗？");
+    }
+}
